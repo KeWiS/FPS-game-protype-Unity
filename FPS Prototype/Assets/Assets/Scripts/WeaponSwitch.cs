b23a578585a4ddc79e3abdd60501cd28564b81e4
Michał Siwek <mjzsiwek@gmail.com>
2020-07-30 03:35:04 +0200

    Added weapon switching

---
+++ FPS Prototype/Assets/Assets/Scripts/WeaponSwitch.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class WeaponSwitch : MonoBehaviour
+{
+    //Public variables
+    public GameObject smallGun;
+    public GameObject sniperRifle;
+    //Private variables
+    bool smallGunDrawed = true;
+	// Update is called once per frame
+	void Update ()
+    {
+        //Checking if player pressed Q key
+		if(Input.GetKeyDown(KeyCode.Q))
+        {
+            //Checking if small gun is drawed, first is true
+            if(smallGunDrawed)
+            {
+                //Changing weapon to sniper rifle
+                smallGun.SetActive(false);
+                sniperRifle.SetActive(true);
+                smallGunDrawed = false;
+            }
+            else
+            {
+                //Changing weapon to small gun
+                sniperRifle.SetActive(false);
+                smallGun.SetActive(true);
+                smallGunDrawed = true;
+            }
+        }
+	}
+}
